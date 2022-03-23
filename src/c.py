f902ff1f2f921c6c0ff67d1ded7eb5af404e1378
spencerlii <spencer850922@gmail.com>
2022-03-23 12:43:37 +0100

    Add files via upload

---
+++ src/c.py
@@ -0,0 +1,51 @@
+from neo4j import GraphDatabase, basic_auth
+import configparser
+import pandas as pd
+import optparse
+
+driver = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "123"))
+
+def create_graph(session):
+    query = """
+    CALL gds.graph.create('myGraph','Paper','Cite')
+    """
+    session.run(query)
+    
+def create_graph2(session):
+    query = """  
+    CALL gds.graph.create('myGraph2','Paper',{
+        Cite: {
+            orientation: 'UNDIRECTED'
+        }
+    })
+    """
+    session.run(query)
+
+    
+def pageRank(session):
+    query = """
+    CALL gds.pageRank.stream('myGraph')
+    YIELD nodeId, score
+    RETURN gds.util.asNode(nodeId).title AS paper,score
+    ORDER BY score DESC
+    """
+    return pd.DataFrame([(record['paper'], record['score']) for record in session.run(query)], columns=['paper','score'])
+
+    
+def triangleCount(session):
+    query = """
+    CALL gds.triangleCount.stream('myGraph2')
+    YIELD nodeId, triangleCount
+    RETURN gds.util.asNode(nodeId).title AS title, triangleCount
+    ORDER BY triangleCount DESC
+    """
+    return pd.DataFrame([(record['title'], record['triangleCount']) for record in session.run(query)], columns=['community','authorList'])
+
+
+with driver.session() as session:
+    create_graph(session)
+    print('1')
+    print(pageRank(session))
+    create_graph2(session)
+    print('2')
+    print(triangleCount(session))
